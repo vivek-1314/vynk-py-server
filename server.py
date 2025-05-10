a6b720dc47f215071d98e44178c3efabbe78fb63
DRE_1314 <vivek1314gurjar@gmail.com>
2025-05-10 17:42:55 +0530

    model changed

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
-model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-distilroberta-base-v1")
 
 class TextInput(BaseModel):
     text: str
